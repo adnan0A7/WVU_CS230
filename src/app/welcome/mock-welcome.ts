54ea9c3d948b17da159e709df0388377e0a9d18c
adnan0A7 <89614479+adnan0A7@users.noreply.github.com>
2021-09-30 18:55:16 -0400

    lab 5

--- src/app/welcome/mock-welcome.ts
+++ src/app/welcome/mock-welcome.ts
@@ -4,7 +4,7 @@ export const mock_card = [
     icon : "./assets/glass.png",
     title : "Find the best answer to your technical question, help others answer theirs",
     button : "Join the community",
-    link : "or search content",
+    link : "search content",
 
 
 
@@ -15,7 +15,7 @@ export const mock_card = [
     icon : "./assets/lock.webp",
     title: "Want a secure, private space for your technical knowledge?",
     button : "Create a free team",
-    link : "or discover teams",
+    link : " discover teams",
 
 
 }
